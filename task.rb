8a787141e6ec2210308d0e5ddb323334f6ca49da
AsukaTameya <tameya.asuka.0121@gmail.com>
2016-01-08 10:33:25 +0900

    7が登場する数をカウント　パターンマッチ

--- task.rb
+++ task.rb
@@ -1,13 +1,9 @@
 while str = STDIN.gets
   n = str.to_i
-
-    if n <70
-      puts n/10+1
-    elsif n % 7 ==0
-      puts (n/10+1)+10*(n.to_s.length-2)+n % (10*(n.to_s.length-1))+1*(n.to_s.length-1)
-    elsif n>=7*10**(n.to_s.length-1) && n<8*10**(n.to_s.length-1)
-        puts (n/10+1)+10*(n.to_s.length-2)+n % (10*(n.to_s.length-1))
-    elsif n>=7*10**(n.to_s.length-1)
-      puts (n/10+1)+10*(n.to_s.length-1)
-    end
+  count = 0
+  for num in 1..n do
+    num_str =num.to_s
+     count += num_str.scan("7").size
+  end
+  puts count
 end
