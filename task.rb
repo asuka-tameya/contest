5ee88b81f271d7793b994a3ba71cfac6afd8066a
AsukaTameya <tameya.asuka.0121@gmail.com>
2016-01-28 01:22:41 +0900

    HelloWorld*1000

--- task.rb
+++ task.rb
@@ -1,9 +1,1 @@
-while str = STDIN.gets
-  n = str.to_i
-  count = 0
-  for num in 1..n do
-    num_str =num.to_s
-     count += num_str.scan("7").size
-  end
-  puts count
-end
+puts "Hello World\n"*1000
