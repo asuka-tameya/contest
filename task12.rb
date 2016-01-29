1ad28b41cf147f726bb9665911f3c7bd186473f4
AsukaTameya <tameya.asuka.0121@gmail.com>
2016-01-29 10:17:46 +0900

    修正

--- task12.rb
+++ task12.rb
@@ -1,8 +1,9 @@
 #11から開始
 num = 11
 while true
-  if (num.to_s == num.to_s.reverse) && (num.to_s(8) == num.to_s(8.reverse) && (num.to_s(2) == num.to_s(2).reverse))
+  if (num.to_s == num.to_s.reverse) && (num.to_s(8) == num.to_s(8).reverse) && (num.to_s(2) == num.to_s(2).reverse)
     puts num
+    end
     #奇数だけ探す
     num += 2
-  end
+end
