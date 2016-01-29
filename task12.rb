ee6c49b2fd4973ac59aa8a5cf479458bfbf42f28
AsukaTameya <tameya.asuka.0121@gmail.com>
2016-01-29 10:20:07 +0900

    １０進数、２進数、８進数のいずれかで表現しても回文数となる数のうち、１０進数の１０以上で最小の値

--- task12.rb
+++ task12.rb
@@ -4,6 +4,6 @@ while true
   if (num.to_s == num.to_s.reverse) && (num.to_s(8) == num.to_s(8).reverse) && (num.to_s(2) == num.to_s(2).reverse)
     puts num
     end
-    #奇数だけ探す
+    #奇数だけ探すため２ずつ増やす
     num += 2
 end
