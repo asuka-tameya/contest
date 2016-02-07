6190a197971d0cf81a9091c344ed8a7a0d0b22f4
AsukaTameya <tameya.asuka.0121@gmail.com>
2016-02-07 14:34:56 +0900

    変数名を変更した

--- task17.rb
+++ task17.rb
@@ -1,11 +1,11 @@
-cnt = 0
+count = 0
 (0..2).each{|coin500|
   (0..10).each{|coin100|
     (0..15).each{|coin50|
       (0..15).each{|coin10|
         if coin500 + coin100 + coin50 + coin10 <= 15 then 
           if coin500 * 500 + coin100 * 100 +coin50 * 50 + coin10 * 10 == 1000 then
-          cnt += 1
+          count += 1
         end
       end
       }
@@ -13,4 +13,4 @@ cnt = 0
   }
 }
 
-puts cnt
+puts count
